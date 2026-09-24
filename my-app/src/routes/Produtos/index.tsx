9b56da5ec7f652e1a43d8421c5ad543d8752974c
MuriloSCruzz <cruzmurilo23@gmail.com>
2026-09-23 21:47:43 -0300

    feat: organizando a tabela de forma intuitiva

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,43 +1,61 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router';
-import { listaProdutos } from '../../data/listaProdutos';
-import type { TipoProduto } from '../../types/types';
+import { useEffect, useState } from "react";
+import type { TipoProduto } from "../../types/types";
+import { listaProdutos } from "../../data/listaProdutos";
+import { Link } from "react-router";
 
 export default function Produtos() {
+    //Modificar o título da página;
     document.title = "Produtos";
+
+    //Criando o recipiente da lista de dados e tipando com o tipo de produto
     const [produtos, setProdutos] = useState<TipoProduto[]>([]);
 
     useEffect(() => {
+        //Simulando a requisição para o backend
         setProdutos(listaProdutos);
+
     }, []);
+
+    for (let index = 0; index < produtos.length; index++) {
+        const element = produtos[index];
+        console.log(element);
+
+    }
 
     return (
         <main style={{ padding: '20px' }}>
-            <h1>Painel de Produtos</h1>
+            <h2>Produtos</h2>
+
             <table border={1} cellPadding={10} style={{ width: '100%', borderCollapse: 'collapse' }}>
                 <thead>
                     <tr style={{ backgroundColor: '#2c3e50', color: '#ffffff' }}>
-                        <th>Foto</th>
                         <th>ID</th>
                         <th>Nome</th>
                         <th>Preço</th>
+                        <th>Descrição</th>
+                        <th>Avatar</th>
                         <th>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {produtos.map((item) => (
-                        <tr key={item.id}>
-                            <td><img src={item.avatar} alt={item.nome} width={60} height={60} style={{ objectFit: 'cover' }} /></td>
-                            <td>{item.id}</td>
-                            <td>{item.nome}</td>
-                            <td>R$ {item.preco.toFixed(2)}</td>
-                            <td>
-                                <Link to={`/editar-produtos/${item.id}`}>Editar</Link>
-                            </td>
+                    {produtos.map((p) => (
+                        <tr key={p.id}>
+                            <td>{p.id}</td>
+                            <td>{p.nome}</td>
+                            <td>{p.preco}</td>
+                            <td>{p.descricao}</td>
+                            <td><img src={p.avatar} alt={p.descricao} width={60} height={60} style={{ objectFit: 'cover' }} /></td>
+                            <td><Link to={`/editar-produtos/${p.id}`}>Editar</Link></td>
                         </tr>
                     ))}
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={6}>Quantidade de produtos - {produtos.length}</td>
+                    </tr>
+                </tfoot>
             </table>
+
         </main>
-    );
+    )
 }

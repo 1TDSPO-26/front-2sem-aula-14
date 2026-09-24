b9f38e7b229efb72433883a2b2adf9dcfc1993fe
theo4321 <theo.caria.r6@gmail.com>
2026-09-23 22:43:25 -0300

    feat:Criação e manipulação de api

--- my-app/src/types/types.ts
+++ my-app/src/types/types.ts
@@ -1,8 +1,15 @@
 export interface TipoProduto{
-    id:number,
+    id:string,
     nome:string,
     preco:number,
     descricao:string,
     avatar:string,
 }
 
+export type TipoProdutoj = {
+    id: string;
+    nome: string;
+    preço: number;
+    estoque: number;
+    avatar: string;
+}
